39f2a64259d8b2321f9e3217dac5b0c605aacc14
DLorell <d.lorell@yahoo.com>
2019-02-16 21:49:58 -0600

    1

--- app/src/main/java/edu/uchicago/lorell/MainActivity.java
+++ app/src/main/java/edu/uchicago/lorell/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.uchicago.lorell;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.app.Activity;
 import android.hardware.Sensor;
@@ -11,12 +12,19 @@ import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
 
+    private static final String TAG = "MainActivity";
 
 
+    private SensorManager sensorManager;
+    Sensor accelerometer;
+
+
+    TextView xAccl, yAccl, zAccl;
 
 
 
@@ -24,5 +32,55 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        xAccl = (TextView) findViewById(R.id.xAccl);
+        yAccl = (TextView) findViewById(R.id.yAccl);
+        zAccl = (TextView) findViewById(R.id.zAccl);
+
+
+
+        Log.d(TAG, "onCreate: Initializing Sensor Services.");
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+        Log.d(TAG, "onCreate: Registered accelerometer listener.");
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(MainActivity.this);
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+        Log.d(TAG, "onSensorChanged: X: " + sensorEvent.values[0] + "  Y: " + sensorEvent.values[1] + "  Z: " + sensorEvent.values[2]);
+
+        xAccl.setText("    xAccl: " + sensorEvent.values[0]);
+        yAccl.setText("    yAccl: " + sensorEvent.values[1]);
+        zAccl.setText("    zAccl: " + sensorEvent.values[2]);
+
+
     }
 }

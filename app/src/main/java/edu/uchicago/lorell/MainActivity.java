2e8486f2948adab118e9a6b5ad3d880b2e2fdb60
DLorell <d.lorell@yahoo.com>
2019-02-18 20:50:05 -0600

    1

--- app/src/main/java/edu/uchicago/lorell/MainActivity.java
+++ app/src/main/java/edu/uchicago/lorell/MainActivity.java
@@ -14,6 +14,14 @@ import android.view.View.OnClickListener;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 import java.io.FileOutputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageInfo;
+import android.os.Environment;
+
 
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener, OnClickListener {
@@ -24,8 +32,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     Double y = 0.0;
     Double z = 0.0;
     String record = "[";
-    Long startTime;
-    Long time;
+    Long startTime = 0L;
+    Long curTime = 0L;
+    Long lastTime = 0L;
+    Float deltat = 0f;
+
+    Double curSpeedx = 0.0;
+    Double lastSpeedx = 0.0;
+    Double curSpeedy = 0.0;
+    Double lastSpeedy = 0.0;
+    Double curSpeedz = 0.0;
+    Double lastSpeedz = 0.0;
+
+    Double xLocPrev = 0.0, yLocPrev = 0.0, zLocPrev = 0.0;
+
 
 
     private SensorManager sensorManager;
@@ -104,15 +124,59 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         Log.d(TAG, "onSensorChanged: X: " + sensorEvent.values[0] + "  Y: " + sensorEvent.values[1] + "  Z: " + sensorEvent.values[2]);
 
 
-        x = Math.floor(sensorEvent.values[0] * 100) / 100;
-        y = Math.floor(sensorEvent.values[1] * 100) / 100;
-        z = Math.floor(sensorEvent.values[2] * 100) / 100;
-        time = (System.currentTimeMillis() - startTime);
+        x = Math.floor(sensorEvent.values[0] * 10) / 10;
+        y = Math.floor(sensorEvent.values[1] * 10) / 10;
+        z = Math.floor(sensorEvent.values[2] * 10) / 10;
 
-        record = record + "{xAccl:" + Double.toString(x) + ", yAccl:"
-                                           + Double.toString(y) + ", zAccl:"
-                                           + Double.toString(z) + ", time:"
-                                           + Long.toString(time) + "},\n";
+        if (x <= 0.1 && x >= -0.1){
+            x = 0d;
+        }
+        if (y <= 0.1 && y >= -0.1){
+            y = 0d;
+        }
+        if (z <= 0.1 && z >= -0.1){
+            z = 0d;
+        }
+
+        lastTime = curTime;
+        curTime = System.currentTimeMillis() - startTime;
+
+        deltat =  new Float(curTime - lastTime) / 1000;
+
+        lastSpeedx = curSpeedx;
+        curSpeedx = lastSpeedx + (x * deltat);
+        lastSpeedy = curSpeedy;
+        curSpeedy = lastSpeedy + (y * deltat);
+        lastSpeedz = curSpeedz;
+        curSpeedz = lastSpeedz + (z * deltat);
+
+        Double xLoc, yLoc, zLoc, deltaXLoc, deltaYLoc, deltaZLoc, xSpeed;
+
+
+        deltaXLoc = (lastSpeedx * deltat) + (x * deltat * deltat / 2);
+        deltaYLoc = (lastSpeedy * deltat) + (y * deltat * deltat / 2);
+        deltaZLoc = (lastSpeedz * deltat) + (z * deltat * deltat / 2);
+
+        xLoc = xLocPrev + deltaXLoc;
+        xLocPrev = xLoc;
+        yLoc = yLocPrev + deltaYLoc;
+        yLocPrev = yLoc;
+        zLoc = zLocPrev + deltaZLoc;
+        zLocPrev = zLoc;
+
+
+
+
+        record = record + "{\"xAccl\":" + Double.toString(x) + ", \"yAccl\":"
+                                           + Double.toString(y) + ", \"zAccl\":"
+                                           + Double.toString(z) + ", \"xSpeed\":"
+                                           + Double.toString(curSpeedx) + ", \"xLoc\":"
+                                           + Double.toString(xLoc) + ", \"yLoc\":"
+                                           + Double.toString(yLoc)+ ", \"zLoc\":"
+                                           + Double.toString(zLoc)+ ", \"time\":"
+                                           + Long.toString(curTime) + ", \"delta_t\":"
+                                           + Float.toString(deltat) +
+                                           "},\n";
 
         String xString = "xAccl: " + Double.toString(x);
         String yString = "yAccl: " + Double.toString(y);
@@ -123,6 +187,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         zAccl.setText(zString);
 
 
+    }
+
+    public File getPrivateStorageDir(Context context, String dirname) {
+        // Get the directory for the app's private pictures directory.
+        File file = new File(context.getExternalFilesDir(
+                Environment.DIRECTORY_DOCUMENTS), dirname);
+        if (!file.mkdirs()) {
+            Log.e(TAG, "Directory not created");
+        }
+        return file;
     }
 
     // This gets called every time you click the button.
@@ -136,6 +210,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if(on) {
             startTime = System.currentTimeMillis();
             sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+
+            try {
+                File filename = new File(this.getFilesDir(),"sensorData2.txt");
+                FileWriter fw = new FileWriter(filename, false);
+                fw.write(new String(new char[100000]).replace("\0", "      \n"));
+                fw.flush();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+
+
         // "Hit Stop Recording"
         }else{
             sensorManager.unregisterListener(this,accelerometer);
@@ -145,15 +230,26 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             yAccl.setText("yAccl: ----");
             zAccl.setText("zAccl: ----");
 
-            String filename = "sensorData.txt";
+
+            File filename = new File(this.getFilesDir(),"sensorData2.txt");
             String fileContents = record + "]";
             FileOutputStream outputStream;
 
+
+
             try {
-                outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+
+
+                FileWriter fw = new FileWriter(filename, false);
+                zAccl.setText(filename.getAbsolutePath());
+                fw.write(fileContents);
+                fw.flush();
+
+                /*
+                outputStream = openFileOutput(filename, );
                 outputStream.write(fileContents.getBytes());
-                outputStream.close();
-                //deleteFile(filename);
+                outputStream.close();*/
+
 
 
             } catch (Exception e) {
@@ -162,19 +258,24 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 
 
+
             //Use this to view the files in the working directory.
             /*
-            *
-            *
+
 
             PackageManager m = getPackageManager();
             String s = getPackageName();
             String x = "";
+            String dir = "";
+            String extdir = "";
 
 
             try {
+
+
+
                 PackageInfo p = m.getPackageInfo(s, 0);
-                s = p.applicationInfo.dataDir;
+                dir = s = p.applicationInfo.dataDir;
                 File directory = new File(s);
                 File[] files = directory.listFiles();
 
@@ -183,19 +284,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     x = x + "\n" + files[i].getName();
                 }
 
-
-
             } catch (PackageManager.NameNotFoundException e) {
                 Log.w("yourtag", "Error Package name not found ", e);
             }
 
+            x = x + "\n" + dir + "\n" + extdir;
 
-            zAccl.setText(x);
             */
+
+
 
         }
 
     }
+
 
 
 }
